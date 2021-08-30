c8e6355390dfb943c5ed4b1d198b4557604c5613
沢谷 邦夫 <devel@fathens.org>
2021-08-30 13:52:29 +0100

    SG90_180 で角度を指定できるようにした

--- src/hardware/i2c/servo.rs
+++ src/hardware/i2c/servo.rs
@@ -1,18 +1,6 @@
 use super::pca9685::{ChannelIndex, HasPrescale, PwmError, PCA9685};
 use pwm_pca9685::Channel;
 
-impl HasPrescale for ServoMotor {
-    fn prescale(&self) -> u8 {
-        self.prescale
-    }
-}
-
-impl ChannelIndex for ServoMotor {
-    fn index(&self) -> Option<u8> {
-        self.channel.index()
-    }
-}
-
 pub struct ServoMotor {
     channel: Channel,
     frequency: f64,
@@ -43,5 +31,54 @@ impl ServoMotor {
         let rate = self.calc_pulse(v);
         pwm.set_prescale(self.prescale)?;
         pwm.set_one_duty_cycle(self.channel, rate)
+    }
+}
+
+impl HasPrescale for ServoMotor {
+    fn prescale(&self) -> u8 {
+        self.prescale
+    }
+}
+
+impl ChannelIndex for ServoMotor {
+    fn index(&self) -> Option<u8> {
+        self.channel.index()
+    }
+}
+
+pub struct SG90_180 {
+    servo: ServoMotor,
+}
+
+impl SG90_180 {
+    pub fn new(channel: Channel, frequency: f64, min_width: f64, max_width: f64) -> SG90_180 {
+        let servo = ServoMotor::new(channel, frequency, min_width, max_width);
+        SG90_180 { servo }
+    }
+
+    fn calc_angle_rate(angle: f64) -> f64 {
+        angle.max(0.0).min(180.0) / 180.0
+    }
+
+    pub fn calc_pulse_by_angle(&self, angle: f64) -> f64 {
+        let v = SG90_180::calc_angle_rate(angle);
+        self.servo.calc_pulse(v)
+    }
+
+    pub fn set_angle(&self, pwm: &mut PCA9685, angle: f64) -> Result<(), PwmError> {
+        let v = SG90_180::calc_angle_rate(angle);
+        self.servo.set_by_rate(pwm, v)
+    }
+}
+
+impl HasPrescale for SG90_180 {
+    fn prescale(&self) -> u8 {
+        self.servo.prescale()
+    }
+}
+
+impl ChannelIndex for SG90_180 {
+    fn index(&self) -> Option<u8> {
+        self.servo.index()
     }
 }
--- src/walk/mod.rs
+++ src/walk/mod.rs
@@ -1,4 +1,4 @@
-use crate::hardware::i2c::servo::ServoMotor;
+use crate::hardware::i2c::servo::SG90_180;
 use crate::hardware::i2c::pca9685::PCA9685;
 use pwm_pca9685::Channel;
 
@@ -8,17 +8,16 @@ pub fn pos_a() {
             println!("Get PCA9685");
             pwm.enable().unwrap();
             let servos = [
-                ServoMotor::new(Channel::C15, 50.0, 0.5, 2.4),
-                ServoMotor::new(Channel::C12, 50.0, 0.5, 2.4),
+                SG90_180::new(Channel::C15, 50.0, 0.5, 2.4),
+                SG90_180::new(Channel::C12, 50.0, 0.5, 2.4),
             ];
             for servo in &servos {
-                servo.set_by_rate(&mut pwm, 0.0).unwrap();
+                servo.set_angle(&mut pwm, 0.0).unwrap();
             }
-            for i in 0..=10 {
-                let r = i as f64 / 10.0;
+            for angle in (0..=180).step_by(10) {
                 pwm.set_duty_cycle(&[
-                    (&servos[0], servos[0].calc_pulse(r)),
-                    (&servos[1], servos[1].calc_pulse(1.0 - r)),
+                    (&servos[0], servos[0].calc_pulse_by_angle(angle as f64)),
+                    (&servos[1], servos[1].calc_pulse_by_angle(180.0 - angle as f64)),
                 ]).unwrap();
                 std::thread::sleep(std::time::Duration::from_millis(500));
             }
