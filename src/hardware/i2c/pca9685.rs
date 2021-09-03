52e3d3a8b7b0085439815d6045da904c9e3e9c5c
沢谷 邦夫 <fsawa@fathens.org>
2021-09-03 18:03:07 +0900

    `group_by` が DST でも動くように改修

--- src/hardware/i2c/pca9685.rs
+++ src/hardware/i2c/pca9685.rs
@@ -81,6 +81,7 @@ impl PCA9685 {
 
 fn group_by<'a, A, B, C, F>(list: &[(&'a A, B)], by: F) -> HashMap<C, Vec<(&'a A, B)>>
 where
+    A: ?Sized,
     B: Copy,
     C: Copy + Eq + std::hash::Hash,
     F: Fn(&A) -> C,
@@ -106,11 +107,20 @@ mod tests {
 
     #[test]
     fn group_by_str() {
-        let list = [(&"A", 10), (&"XA", 30), (&"B", 20), (&"XB", 40)];
+        let list = [("A", 10_u8), ("XA", 30), ("B", 20), ("XB", 40)];
         let result = group_by(&list, |e| e.len());
         assert_eq!(result.len(), 2);
-        assert_eq!(result[&1], vec![(&"A", 10), (&"B", 20)]);
-        assert_eq!(result[&2], vec![(&"XA", 30), (&"XB", 40)]);
+        assert_eq!(result[&1], vec![("A", 10), ("B", 20)]);
+        assert_eq!(result[&2], vec![("XA", 30), ("XB", 40)]);
+    }
+
+    #[test]
+    fn group_by_int() {
+        let list = [(&1_u8, 10_u8), (&2, 20), (&3, 30), (&4, 40)];
+        let result = group_by(&list, |e| e % 2 == 0);
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[&true], vec![(&2, 20), (&4, 40)]);
+        assert_eq!(result[&false], vec![(&1, 10), (&3, 30)]);
     }
 
     #[test]
