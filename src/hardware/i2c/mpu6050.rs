ca0790b0e09482f924211202b2215e39a2bf9db5
沢谷 邦夫 <devel@fathens.org>
2021-09-05 01:46:16 +0100

    nalgebra-0.24 は脆弱性が指摘されたので外す

--- src/hardware/i2c/mpu6050.rs
+++ src/hardware/i2c/mpu6050.rs
@@ -1,7 +1,6 @@
 use i2cdev::linux::LinuxI2CError;
 use linux_embedded_hal::{Delay, I2cdev};
 use mpu6050::*;
-use nalgebra::{Vector2, Vector3};
 
 type MpuError = Mpu6050Error<LinuxI2CError>;
 
@@ -12,9 +11,9 @@ pub struct MPU6050 {
 #[derive(Debug)]
 pub struct MpuInfo {
     temp: f32,
-    gyro: Vector3<f32>,
-    acc: Vector3<f32>,
-    rp: Vector2<f32>,
+    gyro: String,
+    acc: String,
+    rp: String,
 }
 
 impl MPU6050 {
@@ -27,13 +26,13 @@ impl MPU6050 {
 
     pub fn get_infos(&mut self) -> Result<MpuInfo, MpuError> {
         // get roll and pitch estimate
-        let rp = self.inner.get_acc_angles()?;
+        let rp = self.inner.get_acc_angles()?.to_string();
         // get temp
         let temp = self.inner.get_temp()?;
         // get gyro data, scaled with sensitivity
-        let gyro = self.inner.get_gyro()?;
+        let gyro = self.inner.get_gyro()?.to_string();
         // get accelerometer data, scaled with sensitivity
-        let acc = self.inner.get_acc()?;
+        let acc = self.inner.get_acc()?.to_string();
 
         let info = MpuInfo {
             temp,
