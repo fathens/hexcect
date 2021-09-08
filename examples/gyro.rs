34c7ab88118dadbc53552abb64c7a06a8e3f10dc
沢谷 邦夫 <fsawa@fathens.org>
2021-09-08 13:27:41 +0900

    Revert "I2cdev は常にスレッドセーフで使うように変更"
    This reverts commit e6213d56f8921bb61d60775c507d31f3eb73baab.

--- examples/gyro.rs
+++ examples/gyro.rs
@@ -2,12 +2,15 @@ extern crate hexcect;
 
 use hexcect::hardware::i2c::connect;
 use hexcect::hardware::i2c::mpu6050::{Address, MPU6050};
+use parking_lot::Mutex;
 use std::io::stdout;
+use std::sync::Arc;
 
 use crossterm::{cursor::MoveUp, execute, style::Print};
 
 fn main() {
     let dev = connect(1).unwrap();
+    let dev = Arc::new(Mutex::new(dev));
     let mpu = MPU6050::new(dev, Address::LOW).unwrap();
 
     loop {
--- src/hardware/i2c.rs
+++ src/hardware/i2c.rs
@@ -3,32 +3,9 @@ pub mod pca9685;
 pub mod servo;
 
 use i2cdev::linux::LinuxI2CError;
-use lazy_static::*;
 use linux_embedded_hal::I2cdev;
-use parking_lot::Mutex;
-use std::collections::HashMap;
-use std::sync::Arc;
 
-type Result<T> = std::result::Result<T, LinuxI2CError>;
-type ThreadSafeI2c = Arc<Mutex<I2cdev>>;
-
-lazy_static! {
-    static ref I2C_DEVICES: Mutex<HashMap<u8, ThreadSafeI2c>> = Mutex::new(HashMap::new());
-}
-
-/// 指定された bus で既に作られていればそれを使い、無ければ作成する。
-/// 作成した I2cdev は Arc と Mutex でラップして、返すときは常に clone して返す。
-pub fn connect(bus: u8) -> Result<ThreadSafeI2c> {
-    let mut devices = I2C_DEVICES.lock();
-    let result = match devices.get(&bus) {
-        Some(a) => a,
-        None => {
-            let path = format!("/dev/i2c-{}", bus);
-            let dev = I2cdev::new(&path)?;
-            let v = Arc::new(Mutex::new(dev));
-            devices.insert(bus, v);
-            devices.get(&bus).unwrap()
-        }
-    };
-    Ok(Arc::clone(result))
+pub fn connect(bus: u8) -> Result<I2cdev, LinuxI2CError> {
+    let path = format!("/dev/i2c-{}", bus);
+    I2cdev::new(&path)
 }
