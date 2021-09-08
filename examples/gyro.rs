e6213d56f8921bb61d60775c507d31f3eb73baab
沢谷 邦夫 <fsawa@fathens.org>
2021-09-08 13:27:16 +0900

    I2cdev は常にスレッドセーフで使うように変更

--- examples/gyro.rs
+++ examples/gyro.rs
@@ -2,15 +2,12 @@ extern crate hexcect;
 
 use hexcect::hardware::i2c::connect;
 use hexcect::hardware::i2c::mpu6050::{Address, MPU6050};
-use parking_lot::Mutex;
 use std::io::stdout;
-use std::sync::Arc;
 
 use crossterm::{cursor::MoveUp, execute, style::Print};
 
 fn main() {
     let dev = connect(1).unwrap();
-    let dev = Arc::new(Mutex::new(dev));
     let mpu = MPU6050::new(dev, Address::LOW).unwrap();
 
     loop {
