deb45d68bbb9e07e6962083fe6450b2bb18d1296
沢谷 邦夫 <fsawa@fathens.org>
2021-09-12 10:40:43 +0900

    threadsafe ではモックの Clonable を使わないので i2c_register に移動

--- tests/i2c_mock/mod.rs
+++ tests/i2c_mock/mod.rs
@@ -3,9 +3,7 @@ use hexcect::hardware::i2c::register_io::*;
 use core::fmt::Debug;
 use derive_more::{From, Into};
 use embedded_hal::blocking::i2c::{SevenBitAddress, Write, WriteRead};
-use std::cell::RefCell;
 use std::collections::{HashMap, VecDeque};
-use std::rc::Rc;
 
 type I2cAddr = SevenBitAddress;
 
@@ -87,41 +85,4 @@ pub struct MockRegisterC(pub u8);
 
 impl Register for MockRegisterC {
     const ADDR: RegAddr = RegAddr(0x23);
-}
-
-// ----------------------------------------------------------------
-
-pub struct ClonableI2c<T>(pub Rc<RefCell<T>>);
-
-impl<T> ClonableI2c<T> {
-    pub fn new(t: T) -> Self {
-        Self(Rc::new(RefCell::new(t)))
-    }
-}
-
-impl<T> Clone for ClonableI2c<T> {
-    fn clone(&self) -> Self {
-        Self(Rc::clone(&self.0))
-    }
-}
-
-impl<T: Write> Write for ClonableI2c<T> {
-    type Error = <T as Write>::Error;
-
-    fn write(&mut self, address: I2cAddr, bytes: &[u8]) -> Result<(), Self::Error> {
-        self.0.borrow_mut().write(address, bytes)
-    }
-}
-
-impl<T: WriteRead> WriteRead for ClonableI2c<T> {
-    type Error = <T as WriteRead>::Error;
-
-    fn write_read(
-        &mut self,
-        address: I2cAddr,
-        bytes: &[u8],
-        buf: &mut [u8],
-    ) -> Result<(), Self::Error> {
-        self.0.borrow_mut().write_read(address, bytes, buf)
-    }
 }
--- tests/i2c_register.rs
+++ tests/i2c_register.rs
@@ -19,8 +19,9 @@ fn read_registers() {
     assert_eq!(0x78_u8, reg_b.into());
     assert_eq!(0x31_u8, reg_c.into());
 
-    assert_eq!(safe_i2c.0.borrow().written.len(), 1);
-    assert_eq!(safe_i2c.0.borrow().written[&8], vec![0x01, 0x12, 0x23]);
+    let written = safe_i2c.0.borrow().written.clone();
+    assert_eq!(written.len(), 1);
+    assert_eq!(written[&8], vec![0x01, 0x12, 0x23]);
 }
 
 #[test]
@@ -34,11 +35,9 @@ fn write_registers() {
     i2c.write_register(MockRegisterB(0x78)).unwrap();
     i2c.write_register(MockRegisterC(0x31)).unwrap();
 
-    assert_eq!(safe_i2c.0.borrow().written.len(), 1);
-    assert_eq!(
-        safe_i2c.0.borrow().written[&8],
-        vec![0x01, 0x17, 0x12, 0x78, 0x23, 0x31]
-    );
+    let written = safe_i2c.0.borrow().written.clone();
+    assert_eq!(written.len(), 1);
+    assert_eq!(written[&8], vec![0x01, 0x17, 0x12, 0x78, 0x23, 0x31]);
 }
 
 #[test]
@@ -52,7 +51,49 @@ fn read_bytes() {
     let mut buf = [0; 4];
     i2c.read_bytes(RegAddr(0xAC), &mut buf).unwrap();
 
+    let written = safe_i2c.0.borrow().written.clone();
     assert_eq!(buf, [0x41, 0x32, 0x23, 0x14]);
-    assert_eq!(safe_i2c.0.borrow().written.len(), 1);
-    assert_eq!(safe_i2c.0.borrow().written[&7], vec![0xAC]);
+    assert_eq!(written.len(), 1);
+    assert_eq!(written[&7], vec![0xAC]);
+}
+
+// ----------------------------------------------------------------
+use embedded_hal::blocking::i2c::{SevenBitAddress, Write, WriteRead};
+use std::cell::RefCell;
+use std::rc::Rc;
+type I2cAddr = SevenBitAddress;
+
+pub struct ClonableI2c<T>(pub Rc<RefCell<T>>);
+
+impl<T> ClonableI2c<T> {
+    pub fn new(t: T) -> Self {
+        Self(Rc::new(RefCell::new(t)))
+    }
+}
+
+impl<T> Clone for ClonableI2c<T> {
+    fn clone(&self) -> Self {
+        Self(Rc::clone(&self.0))
+    }
+}
+
+impl<T: Write> Write for ClonableI2c<T> {
+    type Error = <T as Write>::Error;
+
+    fn write(&mut self, address: I2cAddr, bytes: &[u8]) -> Result<(), Self::Error> {
+        self.0.borrow_mut().write(address, bytes)
+    }
+}
+
+impl<T: WriteRead> WriteRead for ClonableI2c<T> {
+    type Error = <T as WriteRead>::Error;
+
+    fn write_read(
+        &mut self,
+        address: I2cAddr,
+        bytes: &[u8],
+        buf: &mut [u8],
+    ) -> Result<(), Self::Error> {
+        self.0.borrow_mut().write_read(address, bytes, buf)
+    }
 }
