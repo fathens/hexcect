baae985ba6098f46cc5835f81bd0bc207b350eb1
沢谷 邦夫 <fsawa@fathens.org>
2021-09-13 15:20:15 +0900

    I2C の接続をキャッシュするとき失敗のエラーを文字列として保存する

--- src/hardware/i2c.rs
+++ src/hardware/i2c.rs
@@ -7,36 +7,33 @@ pub mod thread_safe;
 pub use register_io::*;
 pub use thread_safe::*;
 
+use backtrace::Backtrace;
 use derive_more::{From, Into};
 use embedded_hal::blocking::i2c::SevenBitAddress;
 use lazy_static::lazy_static;
 use linux_embedded_hal::I2cdev;
 use parking_lot::Mutex;
 use std::collections::HashMap;
-use std::io::{ErrorKind, Result};
-use backtrace::Backtrace;
 
 type Dev = ThreadSafeI2c<I2cdev>;
+type ResultDev = Result<Dev, String>;
 
 lazy_static! {
-    static ref DEVICES: Mutex<HashMap<u8, Option<Dev>>> = Mutex::new(HashMap::new());
+    static ref DEVICES: Mutex<HashMap<u8, ResultDev>> = Mutex::new(HashMap::new());
 }
 
 #[derive(Debug, From, Into, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct I2cAddr(pub SevenBitAddress);
 
-fn get_connect(path: &str) -> Option<Dev> {
-    match I2cdev::new(&path) {
-        Ok(dev) => Some(thread_safe::ThreadSafeI2c::new(dev)),
-        Err(err) => {
-            eprintln!("Could not connect to I2C {}: {}", path, err);
-            eprintln!("{:?}", Backtrace::new());
-            None
-        }
-    }
+fn get_connect(path: &str) -> ResultDev {
+    I2cdev::new(&path).map(ThreadSafeI2c::new).map_err(|err| {
+        eprintln!("Failed to connect I2C device '{}': {}", path, err);
+        eprintln!("{:?}", Backtrace::new());
+        err.to_string()
+    })
 }
 
-pub fn connect(bus: u8) -> Result<Dev> {
+pub fn connect(bus: u8) -> std::io::Result<Dev> {
     let path = format!("/dev/i2c-{}", bus);
 
     let result = DEVICES
@@ -45,7 +42,5 @@ pub fn connect(bus: u8) -> Result<Dev> {
         .or_insert_with(|| get_connect(&path))
         .clone();
 
-    result.ok_or_else(|| {
-        std::io::Error::new(ErrorKind::NotConnected, format!("No I2C device: {}", path))
-    })
+    result.map_err(|msg| std::io::Error::new(std::io::ErrorKind::NotConnected, msg))
 }
