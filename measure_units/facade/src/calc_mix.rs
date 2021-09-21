6025873b397b0260131a1dbb746bba52c87be825
沢谷 邦夫 <fsawa@fathens.org>
2021-09-21 12:33:31 +0900

    `CalcMix` では scalar_value ではなく `Into` を使うようにした
    `V` を一般的にして Into を実装できないので、個別の方に対して実装するようにマクロ
    を作成した。

--- measure_units/facade/src/calc_mix.rs
+++ measure_units/facade/src/calc_mix.rs
@@ -1,121 +1,156 @@
 use std::fmt::Display;
 use std::lazy::Lazy;
 use std::marker::PhantomData;
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
-pub trait CalcMix {
+#[macro_use]
+mod local_macro {
+    macro_rules! impl_into {
+        ($t:ident, $($v:ident),+) => {
+            $(
+                impl<A, B> Into<$v> for $t<$v, A, B> {
+                    fn into(self) -> $v {
+                        self.0
+                    }
+                }
+            )*
+        };
+    }
+}
+
+pub trait CalcMix<V> {
     const UNIT_NAME: Lazy<String>;
-    type Basis;
-    fn scalar_value(&self) -> Self::Basis;
 }
 
 // ================================================================
 
 #[derive(Clone, Copy)]
-pub struct UnitsMul<V: Copy, A, B>(V, PhantomData<A>, PhantomData<B>);
+pub struct UnitsMul<V, A, B>(V, PhantomData<A>, PhantomData<B>);
 
-impl<V: Copy, A, B> From<V> for UnitsMul<V, A, B> {
+impl_into!(UnitsMul, f32, f64, i32, i64);
+
+impl<V, A, B> From<V> for UnitsMul<V, A, B> {
     fn from(a: V) -> Self {
         Self(a, PhantomData, PhantomData)
     }
 }
 
-impl<V: Copy, A, B> CalcMix for UnitsMul<V, A, B>
+impl<V, A, B> CalcMix<V> for UnitsMul<V, A, B>
 where
-    A: CalcMix,
-    B: CalcMix,
+    A: CalcMix<V>,
+    B: CalcMix<V>,
 {
     const UNIT_NAME: Lazy<String> = Lazy::new(|| format!("{}{}", *A::UNIT_NAME, *B::UNIT_NAME));
-    type Basis = V;
-    fn scalar_value(&self) -> Self::Basis {
-        self.0
-    }
 }
 
 // ----------------------------------------------------------------
 
-impl<V: Copy, A, B> Display for UnitsMul<V, A, B>
+impl<V, A, B> Display for UnitsMul<V, A, B>
 where
-    A: CalcMix,
-    B: CalcMix,
     V: Display,
-    Self: CalcMix<Basis = V>,
+    Self: Copy,
+    Self: Into<V>,
+    Self: CalcMix<V>,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let v = self.scalar_value();
+        let v: V = (*self).into();
         write!(f, "{}{}", v, *Self::UNIT_NAME)
     }
 }
 
-impl<V: Copy, A, B> Add for UnitsMul<V, A, B>
+impl<V, A, B> Add for UnitsMul<V, A, B>
 where
-    A: CalcMix,
-    B: CalcMix,
     V: Add,
-    V: 'static,
-    Self: From<<V as Add>::Output>,
+    Self: Into<V>,
+    Self: From<V::Output>,
 {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let x: V = self.scalar_value();
-        let y: V = rhs.scalar_value();
+        let x: V = self.into();
+        let y: V = rhs.into();
         (x + y).into()
+    }
+}
+
+impl<V, A, B> Sub for UnitsMul<V, A, B>
+where
+    V: Sub,
+    Self: Into<V>,
+    Self: From<V::Output>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let x: V = self.into();
+        let y: V = rhs.into();
+        (x - y).into()
     }
 }
 
 // ================================================================
 
 #[derive(Clone, Copy)]
-pub struct UnitsDiv<V: Copy, A, B>(V, PhantomData<A>, PhantomData<B>);
+pub struct UnitsDiv<V, A, B>(V, PhantomData<A>, PhantomData<B>);
 
-impl<V: Copy, A, B> From<V> for UnitsDiv<V, A, B> {
+impl_into!(UnitsDiv, f32, f64, i32, i64);
+
+impl<V, A, B> From<V> for UnitsDiv<V, A, B> {
     fn from(a: V) -> Self {
         Self(a, PhantomData, PhantomData)
     }
 }
 
-impl<V: Copy, A, B> CalcMix for UnitsDiv<V, A, B>
+impl<V, A, B> CalcMix<V> for UnitsDiv<V, A, B>
 where
-    A: CalcMix,
-    B: CalcMix,
+    A: CalcMix<V>,
+    B: CalcMix<V>,
 {
     const UNIT_NAME: Lazy<String> = Lazy::new(|| format!("{}/{}", *A::UNIT_NAME, *B::UNIT_NAME));
-    type Basis = V;
-    fn scalar_value(&self) -> Self::Basis {
-        self.0
-    }
 }
 
 // ----------------------------------------------------------------
 
-impl<V: Copy, A, B> Display for UnitsDiv<V, A, B>
+impl<V, A, B> Display for UnitsDiv<V, A, B>
 where
-    A: CalcMix,
-    B: CalcMix,
     V: Display,
-    Self: CalcMix<Basis = V>,
+    Self: Copy,
+    Self: Into<V>,
+    Self: CalcMix<V>,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let v = self.scalar_value();
+        let v: V = (*self).into();
         write!(f, "{}{}", v, *Self::UNIT_NAME)
     }
 }
 
-impl<V: Copy, A, B> Add for UnitsDiv<V, A, B>
+impl<V, A, B> Add for UnitsDiv<V, A, B>
 where
-    A: CalcMix,
-    B: CalcMix,
     V: Add,
-    V: 'static,
-    Self: From<<V as Add>::Output>,
+    Self: Into<V>,
+    Self: From<V::Output>,
 {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let x: V = self.scalar_value();
-        let y: V = rhs.scalar_value();
+        let x: V = self.into();
+        let y: V = rhs.into();
         (x + y).into()
+    }
+}
+
+impl<V, A, B> Sub for UnitsDiv<V, A, B>
+where
+    V: Sub,
+    Self: Into<V>,
+    Self: From<V::Output>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let x: V = self.into();
+        let y: V = rhs.into();
+        (x - y).into()
     }
 }
 
@@ -125,23 +160,18 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use derive_more::Into;
 
+    #[derive(Clone, Copy, Into)]
     pub struct Meter(f64);
-    impl CalcMix for Meter {
+    impl CalcMix<f64> for Meter {
         const UNIT_NAME: Lazy<String> = Lazy::new(|| "m".to_string());
-        type Basis = f64;
-        fn scalar_value(&self) -> Self::Basis {
-            self.0
-        }
     }
 
+    #[derive(Clone, Copy, Into)]
     pub struct Second(f64);
-    impl CalcMix for Second {
+    impl CalcMix<f64> for Second {
         const UNIT_NAME: Lazy<String> = Lazy::new(|| "s".to_string());
-        type Basis = f64;
-        fn scalar_value(&self) -> Self::Basis {
-            self.0
-        }
     }
 
     #[test]
@@ -149,7 +179,16 @@ mod tests {
         let a = UnitsDiv::<f64, Meter, Second>::from(1.2);
         let b = UnitsDiv::<f64, Meter, Second>::from(3.4);
         let c = a + b;
-        assert_eq!(c.scalar_value(), 4.6);
+        assert_eq!(c.0, 4.6);
         assert_eq!(c.to_string(), "4.6m/s");
+    }
+
+    #[test]
+    fn simple_sub() {
+        let a = UnitsDiv::<f64, Meter, Second>::from(1.2);
+        let b = UnitsDiv::<f64, Meter, Second>::from(3.5);
+        let c = a - b;
+        assert_eq!(c.0, -2.3);
+        assert_eq!(c.to_string(), "-2.3m/s");
     }
 }
