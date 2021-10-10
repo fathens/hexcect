313d0e9306d222125b6f06628af92eed4b303d89
沢谷 邦夫 <fsawa@fathens.org>
2021-10-10 15:52:15 +0900

    現在位置の更新を計算

--- controller/src/motion.rs
+++ controller/src/motion.rs
@@ -14,7 +14,7 @@ use num_traits::{Float, FromPrimitive};
 pub struct Posture<V: Float> {
     gravity: Accel3D<V>,
     angle: Radians3D<V>,
-    pos: Position3D<Millimeters<V>>,
+    pos: Position3D<Meters<V>>,
     movement: Vector3D<Speed<V>>,
     prev_accel: Accel3D<V>,
     prev_gyro: Gyro3D<V>,
@@ -49,6 +49,7 @@ impl<V: Float> Posture<V> {
         V: From<AngleVelocity<V>>,
         V: From<Accel<V>>,
         V: From<Speed<V>>,
+        V: From<Meters<V>>,
         V: From<Seconds<V>>,
         V: From<Milliseconds<V>>,
         V: From<Nanoseconds<V>>,
@@ -58,20 +59,25 @@ impl<V: Float> Posture<V> {
         let rotate_epsilon = self
             .prev_gyro
             .combine(&gyro, |p, n| integral_dur(dur, p, n));
-        let delta = gyro_delta(&self.angle, &rotate_epsilon.into());
-        let next_gravigy = rotate(&self.gravity, &delta);
+        let angle_delta = gyro_delta(&self.angle, &rotate_epsilon.into());
+        let next_gravigy = rotate(&self.gravity, &angle_delta);
 
         let next_accel = accel - &self.gravity;
-        let speed = self
+        let speed_delta = self
             .prev_accel
             .combine(&next_accel, |p, n| integral_dur(dur, p, n));
+
+        let next_movement = speed_delta + &self.movement;
+        let move_delta = self
+            .movement
+            .combine(&next_movement, |p, n| integral_dur(dur, p, n));
 
         // TODO 仮の戻り値
         Self {
             gravity: next_gravigy,
-            angle: self.angle + &delta,
-            pos: self.pos,
-            movement: self.movement + &speed,
+            angle: self.angle + &angle_delta,
+            pos: self.pos + &move_delta,
+            movement: next_movement,
             prev_accel: next_accel,
             prev_gyro: gyro,
             timestamp: next_timestamp,
